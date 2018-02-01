0a3b237aa2f0fd552c6559431ae6c96e3a022f15
GilRa1731814 <rara1433@hotmail.com>
2018-02-01 14:19:08 -0500

    v8

---
+++ src/adresse.java
@@ -0,0 +1,25 @@
+/**
+ * Created by GilRa1731814 on 2018-01-22.
+ */
+public class adresse {
+    public String numadresse;
+    String nomderue;
+    String appartement;
+    String ville;
+    String province;
+    String pays;
+
+    public String getNomderue (){return this.nomderue;}
+    public void setNomderue (String _nomderue){this.nomderue=_nomderue;}
+    public String getNumadresse(){return this.numadresse;}
+    public void setNumadresse (String _numadresse){this.numadresse=_numadresse;}
+    public String getAppartement (){return this.appartement;}
+    public void setAppartement (String _appartement){this.appartement=_appartement;}
+    public String getVille (){return this.ville;}
+    public void setVille (String _ville){this.ville=_ville;}
+    public String getProvince (){return this.province;}
+    public void setProvince (String _province){this.province=_province;}
+    public String getPays (){return this.pays;}
+    public void setpays(String _pays){this.pays=_pays;}
+
+}
